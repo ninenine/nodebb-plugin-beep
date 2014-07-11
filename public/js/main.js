a9ce754dc86734c7555ba861550e74a92b07b682
Davis Wainaina <davisjwn@gmail.com>
2014-07-11 17:39:49 +0300

    laugh at my pain

--- public/js/main.js
+++ public/js/main.js
@@ -78,6 +78,7 @@
       ];
     });
     $(window) .on('action:ajaxify.end', function (ev, data) {
+      //TODO data.url.match(/^search/)
       if (data.url.match(/^category/) || data.url.match(/^unread/) || data.url.match(/^recent/) || data.url.match(/^popular/)) {
         censorTopics();
       }
@@ -98,32 +99,68 @@
         hidesting += '*';
       }
       var re2 = new RegExp(badwords[w].substring(1, badwords[w].length - 1), 'ig');
-      var hashword = badwords[w].replace(re2, hidesting)
       //Change topic title on topic list
       $('.category-item .topic-title') .each(function () {
         if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
           $(this) .html($(this) .html() .replace(re, hashword));
         }
       });
       //Change topic title on topic
       $('h3.topic-title p.topic-title') .each(function () {
         if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
           $(this) .html($(this) .html() .replace(re, hashword));
         }
       });
       //Change Breadcrump
       $('ol.breadcrumb li.active span') .each(function () {
         if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
           $(this) .html($(this) .html() .replace(re, hashword));
         }
       });
+      /* Causes stuff(random blinking on the homepage) to happen for some reason */
+      /*
+      //Change recent replies panel information
+      $('.panel .recent-replies') .each(function () {
+        if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
+          $(this) .html($(this) .html() .replace(re, hashword));
+        }
+      });
+      */
+  
+      /* Won't work because of highlighting */
+      /*
+      //Change search information
+      $('.search-result-text') .each(function () {
+        if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
+          $(this) .html($(this) .html() .replace(re, hashword));
+        }
+      });
+      */
+
       //Change header information
       $('.header-topic-title span') .each(function () {
         if ($(this) .html() .match(re)) {
+          var match = $(this) .html() .match(re);        
+          var hashword = match[0].replace(re2, hidesting);
           $(this) .html($(this) .html() .replace(re, hashword));
         }
       });
-      document.title = document.title.replace(re, hashword);
+      
+      if (document.title.match(re)){
+        var match = document.title.match(re);        
+        var hashword = match[0].replace(re2, hidesting);
+        document.title = document.title.replace(re, hashword);
+      }
     }
   }
 }());
