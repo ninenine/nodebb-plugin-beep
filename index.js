533c90387743926accc6f1ea5443d5f4794dd06b
Davis Wainaina <davisjwn@gmail.com>
2014-07-11 12:51:48 +0300

    added ACP

--- index.js
+++ index.js
@@ -1,78 +1,42 @@
 (function (module) {
   'use strict';
-  var Beep = {};
+  var Beep = {
+    banned_words: undefined
+  };
+  var fs = require('fs'),
+  path = require('path'),
+  winston = module.parent.require('winston'),
+  Meta = module.parent.require('./meta'),
+  db = module.parent.require('./database');
+
+  Beep.init = function(app, middleware, controllers) {
+    function render(req, res, next) {
+      res.render('admin/plugins/beep', {});
+    }
+
+    app.get('/admin/plugins/beep', middleware.admin.buildHeader, render);
+    app.get('/api/admin/plugins/beep', render);
+    app.get('/api/plugins/beep', function(req, res) {
+      if (Beep.banned_words) {
+        res.send(200, Beep.banned_words);
+      } else {
+        res.send(501);
+      }
+    });
+
+    // Load Banned Words from config
+    Meta.settings.getOne('beep', 'id', function(err, banned_words) {
+      if (!err && banned_words && banned_words.length) {
+        Beep.banned_words = banned_words;
+      } else {
+        Beep.banned_words = "anal,anus,arse,ass,ballsack,balls,bastard,bitch,biatch,bloody,blowjob,blow job,bollock,bollok,boner,boob,bugger,bum,butt,buttplug,clitoris,cock,coon,crap,cunt,damn,dick,dildo,dyke,fag,feck,fellate,fellatio,felching,fuck,f u c k,fudgepacker,fudge packer,flange,homo,jerk,jizz,knobend,knob end,labia,muff,nigger,nigga,penis,piss,poop,prick,pube,pussy,queer,sex,shit,s hit,sh1t,slut,smegma,spunk,tit,tosser,turd,twat,vagina,wank,whore";
+        //winston.error('List of Banned Words was not specified. Please complete setup in the administration panel.');
+      }
+    });
+  };
+
   Beep.parse = function (postContent, callback) {
-    var badwords = [
-      'anal',
-      'anus',
-      'arse',
-      'ass',
-      'ballsack',
-      'balls',
-      'bastard',
-      'bitch',
-      'biatch',
-      'bloody',
-      'blowjob',
-      'blow job',
-      'bollock',
-      'bollok',
-      'boner',
-      'boob',
-      'bugger',
-      'bum',
-      'butt',
-      'buttplug',
-      'clitoris',
-      'cock',
-      'coon',
-      'crap',
-      'cunt',
-      'damn',
-      'dick',
-      'dildo',
-      'dyke',
-      'fag',
-      'feck',
-      'fellate',
-      'fellatio',
-      'felching',
-      'fuck',
-      'f u c k',
-      'fudgepacker',
-      'fudge packer',
-      'flange',
-      'homo',
-      'jerk',
-      'jizz',
-      'knobend',
-      'knob end',
-      'labia',
-      'muff',
-      'nigger',
-      'nigga',
-      'penis',
-      'piss',
-      'poop',
-      'prick',
-      'pube',
-      'pussy',
-      'queer',
-      'sex',
-      'shit',
-      's hit',
-      'sh1t',
-      'slut',
-      'smegma',
-      'spunk',
-      'tit',
-      'tosser',
-      'turd',
-      'twat',
-      'vagina',
-      'wank',
-      'whore'
-    ];
+    badwords = Beep.banned_words.split(',');
     for (var w in badwords) {
       var re = new RegExp(badwords[w], 'ig');
       var hidesting = '';
@@ -87,5 +51,18 @@
     }
     callback(null, postContent);
   };
+
+  Beep.admin = {
+    menu: function(custom_header, callback) {
+      custom_header.plugins.push({
+        "route": '/plugins/beep',
+        "icon": 'fa-microphone-slash',
+        "name": 'Censor Curse Words'
+      });
+
+      callback(null, custom_header);
+    }
+  };
+
   module.exports = Beep;
 }(module));
---
+++ public/templates/admin/plugins/beep.tpl
@@ -0,0 +1,44 @@
+<h1><i class="fa fa-bar-chart-o"></i>NodeBB Censor Curse Words Plugin</h1>
+
+<div class="alert alert-info">
+	<p>
+		This is a NodeBB Plugin that allows users to censor curse words in their posts.
+	</p>
+</div>
+
+<form role="form" class="beep-settings">
+	<fieldset>
+		<div class="form-group">
+			<label for="id"><i>comma-separated</i> list of Banned Words</label>
+			<input type="text" class="form-control" id="id" name="id" placeholder="eg. anal,anus,arse,ass,ballsack" />
+		</div>
+		<p class="help-block">
+			if invalid entry, default is:
+		</p>
+		<pre>
+		anal,anus,arse,ass,ballsack,balls,bastard,bitch,biatch,bloody,blowjob,blow job,bollock,bollok,boner,boob,bugger,bum,butt,buttplug,clitoris,cock,coon,crap,cunt,damn,dick,dildo,dyke,fag,feck,fellate,fellatio,felching,fuck,f u c k,fudgepacker,fudge packer,flange,homo,jerk,jizz,knobend,knob end,labia,muff,nigger,nigga,penis,piss,poop,prick,pube,pussy,queer,sex,shit,s hit,sh1t,slut,smegma,spunk,tit,tosser,turd,twat,vagina,wank,whore
+		</pre>
+
+		<button class="btn btn-lg btn-primary" id="save" type="button">Save</button>
+	</fieldset>
+</form>
+
+<script type="text/javascript">
+	require(['settings'], function(Settings) {
+		Settings.load('beep', $('.beep-settings'));
+
+		$('#save').on('click', function() {
+			Settings.save('beep', $('.beep-settings'), function() {
+				app.alert({
+					type: 'success',
+					alert_id: 'beep-saved',
+					title: 'Restart Required',
+					message: 'Please restart your NodeBB to complete configuration of this plugin',
+					clickfn: function() {
+						socket.emit('admin.restart');
+					}
+				});
+			});
+		});
+	});
+</script>
