83c7116661d6be98b3ef4679f857df21d1de543d
Barış Soner Uşaklı <barisusakli@gmail.com>
2016-10-12 18:44:14 +0300

    success is deprecated in jquery

--- static/main.js
+++ static/main.js
@@ -2,7 +2,7 @@
     'use strict';
     var badwords = [];
     jQuery('document').ready(function() {
-        $.get(RELATIVE_PATH + '/api/plugins/beep').success(function(banned_words) {
+        $.get(RELATIVE_PATH + '/api/plugins/beep').done(function(banned_words) {
             badwords = banned_words.split(',');
         }).fail(function() {
             badwords = [
