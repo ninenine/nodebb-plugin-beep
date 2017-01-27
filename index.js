7dc546ba292be6d6d0fc9531f18cf2faa8d1cfda
Julian Lam <julian@nodebb.org>
2017-01-27 12:19:08 -0500

    handling infinite load of category topics

--- index.js
+++ index.js
@@ -181,6 +181,19 @@
                 });
 
                 callback(null, data);
+            },
+
+            topics: {
+                get: function (data, callback) {
+                    console.log(data);
+                    data.topics.forEach(function (topic) {
+                        topic.title = Beep.parseContent(topic.title);
+                        topic.slug = Beep.parseContent(topic.slug);
+                        topic.titleRaw = Beep.parseContent(topic.titleRaw);
+                    });
+
+                    callback(null, data);
+                }
             }
         },
 
