e8945bb0749dabfb5cedf512b23f9c711f9e5093
Peter Jaszkowiak <p.jaszkow@gmail.com>
2017-03-28 00:20:22 -0600

    Unicode support, settings updated correctly

--- tests/toRegExp.spec.js
+++ tests/toRegExp.spec.js
@@ -4,6 +4,9 @@ var assert = require('assert');
 
 var toRegExp = require('../lib/toRegExp');
 
-assert.deepStrictEqual(toRegExp(['bad', 'words', 'here']), 'bad|words|here');
-assert.deepStrictEqual(toRegExp('bad,words, here'), 'bad|words|here');
-assert.deepStrictEqual(toRegExp(null), '^(?!x)x');
+assert.equal(toRegExp(['bad', 'words', 'here'], true), '/\\b(?:bad|words|here)\\b/gi');
+assert.equal(toRegExp('bad,words, here'), '/bad|words|here/gi');
+assert.equal(toRegExp('今,野', true), '/今|野/gi');
+assert.equal(toRegExp(null), '/^(?!x)x/gi');
+
+console.log('toRegExp passed');
